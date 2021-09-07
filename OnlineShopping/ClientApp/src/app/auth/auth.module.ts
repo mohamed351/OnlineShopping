5142a8c40dd0d19ddbbc1857f61b8635350bd5ea
Mohamed Amer <mohamed.perry351@gmail.com>
2021-09-07 19:37:22 +0200

    Add login Design

--- OnlineShopping/ClientApp/src/app/auth/auth.module.ts
+++ OnlineShopping/ClientApp/src/app/auth/auth.module.ts
@@ -3,17 +3,29 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthicationRoutingModule } from './auth.router.module';
-
-
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+
   ],
   imports: [
     CommonModule,
-    AuthicationRoutingModule
+    AuthicationRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule
+
   ]
 })
 export class AuthModule { }
